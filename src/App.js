150134614d78c07fd956745ee017b1b959143124
santhosh s <subramanisanthosh00@gmail.com>
2023-03-18 19:31:50 +0530

    Rounting is working good

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Header from "./components/Header/Header";
 import Home from "./components/pages/home/Home";
 import Login from "./components/pages/login/Login";
 import Product from "./components/pages/products/Product";
-import ProductDetails from "./components/pages/products/ProductDetails";
-import Nav from "./components/pages/products/Nav";
 import User from "./components/pages/user/User";
 import ContactUs from "./components/pages/contact/ContactUs";
 import { Routes, Route } from "react-router-dom";
@@ -41,13 +39,10 @@ function App() {
       <div className="app">
         <Header />
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/product/pd" element={<ProductDetails />} />
-          <Route path="/product/nav" element={<Nav />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/contact" element={<ContactUs />} />
+          <Route path="product" element={<Product />} />
+          <Route path="user" element={<User />} />
+          <Route path="contact" element={<ContactUs />} />
         </Routes>
       </div>
     </GoblaData.Provider>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,11 +35,11 @@ function Header() {
 
             <li><Link className='a' onClick={() => { linkClicked("home") }} to="/" style={(linkStatus.home) ? (linkStyle.active) : (linkStyle.notActive)}>Home <span style={(linkStatus.home) ? (linkStyle.dot) : (linkStyle.dotHidde)}></span></Link></li>
 
-            <li><Link className='a' onClick={() => { linkClicked("product") }} to="/product" style={(linkStatus.product) ? (linkStyle.active) : (linkStyle.notActive)}>Products <span style={(linkStatus.product) ? (linkStyle.dot) : (linkStyle.dotHidde)}></span></Link></li>
+            <li><Link className='a' onClick={() => { linkClicked("product") }} to="product" style={(linkStatus.product) ? (linkStyle.active) : (linkStyle.notActive)}>Products <span style={(linkStatus.product) ? (linkStyle.dot) : (linkStyle.dotHidde)}></span></Link></li>
 
-            <li><Link className='a' onClick={() => { linkClicked("user") }} to="/user" style={(linkStatus.user) ? (linkStyle.active) : (linkStyle.notActive)}>User <span style={(linkStatus.user) ? (linkStyle.dot) : (linkStyle.dotHidde)}></span></Link></li>
+            <li><Link className='a' onClick={() => { linkClicked("user") }} to="user" style={(linkStatus.user) ? (linkStyle.active) : (linkStyle.notActive)}>User <span style={(linkStatus.user) ? (linkStyle.dot) : (linkStyle.dotHidde)}></span></Link></li>
 
-            <li><Link className='a' onClick={() => { linkClicked("contact") }} to="/contact" style={(linkStatus.contact) ? (linkStyle.active) : (linkStyle.notActive)}>Contact <span style={(linkStatus.contact) ? (linkStyle.dot) : (linkStyle.dotHidde)}></span></Link></li>
+            <li><Link className='a' onClick={() => { linkClicked("contact") }} to="contact" style={(linkStatus.contact) ? (linkStyle.active) : (linkStyle.notActive)}>Contact <span style={(linkStatus.contact) ? (linkStyle.dot) : (linkStyle.dotHidde)}></span></Link></li>
 
           </ul>
           <button className='logout-btn'>Logout</button>
